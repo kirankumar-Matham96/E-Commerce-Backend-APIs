d46e5fb45b36bebd18631c28f9c9c426d1a27faa
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-05-30 02:08:38 +0530

    feat: added dotenv for secrets

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -1,4 +1,6 @@
 import UserModel from "./user.model.js";
+import jwt from "jsonwebtoken";
+require("dotenv").config();
 
 class UserController {
   getAllUsers = (req, res) => {
@@ -27,9 +29,17 @@ class UserController {
   // };
 
   userSignIn = (req, res) => {
+
+    
     const userFound = UserModel.login(req.body);
     if (userFound && userFound.password === req.body.password) {
-      return res.status(200).json({ status: "success", message: "login success!" });
+      
+      // creating jwt
+      const jwtToken = jwt.sign({id: userFound.id, email: userFound.email}, process.env.SECRET_KEY, {
+        expiresIn: "1h"
+      });
+
+      return res.status(200).json({ status: "success", message: "login success!", token: jwtToken });
     }
     return res.status(401).json({ status: "failure", error: "Invalid credentials" });
   };
