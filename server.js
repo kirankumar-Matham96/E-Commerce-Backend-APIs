5d2fa63635f0ba6b5ec1ea87324c78989d67b9fe
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-05-31 11:25:00 +0530

    feat: added swagger UI documentation

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from "express";
+import fs from "fs";
+import path from "path";
 import cookiesParser from "cookie-parser";
+import swagger from "swagger-ui-express";
+
 import ProductRouter from "./src/features/product/products.routes.js";
 import UserRouter from "./src/features/user/user.routes.js";
 import CartRouter from "./src/features/cart/cart.routes.js";
 // import basicAuthorizer from "./src/middleware/basicAuth.middleware.js";
 import { jwtAuth } from "./src/middleware/jwtAuth.middleware.js";
+
+// reading swagger config file
+const swaggerFilePath = path.join(path.resolve(), "swagger.json");
+const apiDocs = JSON.parse(fs.readFileSync(swaggerFilePath, "utf8"));
 
 const PORT = 3600;
 const app = express();
@@ -18,6 +26,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookiesParser());
+
+// swagger-docs api
+app.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
 
 // for all the requests related to products, redirect to product routes.
 app.use("/api/products", jwtAuth, ProductRouter);
