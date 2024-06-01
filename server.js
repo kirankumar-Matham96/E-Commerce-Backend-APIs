5ff92632be89f77ac7f6e980c4b5528dea900f21
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-06-01 22:27:34 +0530

    refracted: added winston library to log the data

--- server.js
+++ server.js
@@ -21,16 +21,6 @@ const app = express();
 
 // cors policy config
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "*");
-//   res.header("Access-Control-Allow-Methods", "*");
-//   // return ok for preflight request
-//   if(req.method ==="OPTIONS"){
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
 
 // swagger-docs api
 app.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
--- src/middleware/logger.middleware.js
+++ src/middleware/logger.middleware.js
@@ -1,20 +1,16 @@
-import fs from "fs";
+import winston from "winston";
 
-const fsPromise = fs.promises;
-
-const log = async (logData) => {
-  try {
-    logData = new Date().toString() + ". Log Data: " + logData + ";\n";
-    await fsPromise.appendFile("log.txt", logData);
-  } catch (error) {
-    console.log(error);
-  }
-};
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "Request - logging" },
+  transports: [new winston.transports.File({ filename: "logs.txt" })],
+});
 
 export const loggerMiddleware = async (req, res, next) => {
   if (!req.url.includes("login") && !req.url.includes("register")) {
     const logData = `${req.url} - ${JSON.stringify(req.body)}`;
-    await log(logData);
+    logger.info(logData);
   }
   next();
 };
